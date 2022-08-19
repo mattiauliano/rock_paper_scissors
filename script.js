94f2e26cdd5d77087bf879947c2bc3d27af7cc65
Mattia Uliano <mattiauliano308@gmail.com>
2022-08-19 23:06:19 +0200

    Add the possibility of playing a round

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-let options = [
-    "Rock",
-    "Paper",
-    "Scissors"
-];
+let cpuScore = 0;
+let playerScore = 0;
+let playerSelection;
+let cpuSelection;
 
-function getComputerChoice() {
-    let randomSelection = options[Math.floor(Math.random()*options.length)]
-    return randomSelection;
+function getRandomChoice() {
+    let options = ["rock", "paper", "scissors"]
+    cpuSelection = options[Math.floor(Math.random()*options.length)];
+    return cpuSelection
 }
 
-let computerSelection = getComputerChoice()
-
-function playerSelection() {
-    let playerChoice = window.prompt("Choose! Rock, Paper or Scissors?")
+function getPlayerChoice() {
+    const playerChoice = prompt("Choose! Rock, Paper or Scissors?")
+    return playerSelection = playerChoice.toLowerCase()
 }
 
+function playRound(playerSelection, cpuSelection) {
+
+    getRandomChoice()
+    getPlayerChoice()
+
+    if((playerSelection=="rock" && cpuSelection=="scissors") || 
+       (playerSelection=="scissors" && cpuSelection=="paper") ||
+       (playerSelection=="paper" && cpuSelection=="rock")) {
+            alert("You WON!");
+            playerScore = ++playerScore
+            console.log(playerScore)
+    } else if((playerSelection=="rock" && cpuSelection=="paper") || 
+              (playerSelection=="scissors" && cpuSelection=="rock") ||
+              (playerSelection=="paper" && cpuSelection=="scissors")) {
+            alert("You LOST!");
+            cpuScore = ++cpuScore
+            console.log(cpuScore)
+    } else {
+        alert("It's TIE!")
+    }
+}
