bdc427b649a4a483962c4110614db01af1a9524e
Mattia Uliano <mattiauliano308@gmail.com>
2022-08-23 10:33:59 +0200

    Integrate dom manipulation in the script

--- script.js
+++ script.js
@@ -1,43 +1,106 @@
-let cpuScore = 0;
-let playerScore = 0;
-let playerSelection;
-let cpuSelection;
+const choices = document.querySelectorAll(".choice");
+const modalResult = document.getElementById("round-result");
+const modal = document.querySelector(".modal");
+let playerScoreIncrement = document.getElementById("player-score");
+let cpuScoreIncrement = document.getElementById("cpu-score");
 
+// Use an array to avoid bugs with playerScore = 0; and cpuScore = 0;
+const scoreboard = {
+    player: 0,
+    cpu: 0
+}
+
+let playerSelection;
+
+// Start a game
+function play(e) {
+    // Get player selection
+    const playerSelection = e.target.id;
+    const cpuSelection = getRandomChoice();
+    const winner = getWinner(playerSelection, cpuSelection);
+    const scoresDisplay = incrementScores(winner, cpuSelection);
+    const restart = resetScores(scoreboard);
+}
+
+// Get a random CPU selection
 function getRandomChoice() {
-    let options = ["rock", "paper", "scissors"]
-    cpuSelection = options[Math.floor(Math.random()*options.length)];
+    const options = ["rock", "paper", "scissors"]
+    const cpuSelection = options[Math.floor(Math.random()*options.length)];
     return cpuSelection
 }
 
-function getPlayerChoice() {
-    const playerChoice = prompt("Choose! Rock, Paper or Scissors?")
-    return playerSelection = playerChoice.toLowerCase()
+// Get a winner in a round
+function getWinner(playerSelection, cpuSelection) {
+    if (playerSelection === cpuSelection) {
+        return "draw"
+    } else if (playerSelection === "rock") {
+        if (cpuSelection === "scissors") {
+            return "player"
+        } else {
+            return "cpu"
+        }
+    } else if (playerSelection === "paper") {
+        if (cpuSelection === "rock") {
+            return "player"
+        } else {
+            return "cpu"
+        }
+    } else if (playerSelection === "scissors") {
+        if (cpuSelection === "paper") {
+            return "player"
+        } else {
+            return "cpu"
+        }
+    }
 }
 
-function playRound(playerSelection, cpuSelection) {
+// Scores increase and show the modal
+function incrementScores(winner, cpuSelection) {
 
-    getRandomChoice()
-    getPlayerChoice()
-
-    if((playerSelection=="rock" && cpuSelection=="scissors") || 
-       (playerSelection=="scissors" && cpuSelection=="paper") ||
-       (playerSelection=="paper" && cpuSelection=="rock")) {
-            alert("You WON!");
-            playerScore = ++playerScore
-            console.log(playerScore)
-    } else if((playerSelection=="rock" && cpuSelection=="paper") || 
-              (playerSelection=="scissors" && cpuSelection=="rock") ||
-              (playerSelection=="paper" && cpuSelection=="scissors")) {
-            alert("You LOST!");
-            cpuScore = ++cpuScore
-            console.log(cpuScore)
+    //Score increase and add modal texts
+    if (winner==="player") {
+        scoreboard.player++
+        playerScoreIncrement.innerHTML = `<p class="player-score">${scoreboard.player}</p>`;
+        modalResult.innerHTML = `
+            <h1>You WIN!</h1>
+            <i class="choice fas fa-hand-${cpuSelection} fa-10x style="color:#05386b></i>
+            <p><strong>CPU Chose ${cpuSelection}</strong></p>
+        `;
+    } else if (winner==="cpu") {
+        scoreboard.cpu++
+        cpuScoreIncrement.innerHTML = `<p class="cpu-score">${scoreboard.cpu}</p>`;
+        modalResult.innerHTML = `
+            <h1>You LOSE!</h1>
+            <i class="choice fas fa-hand-${cpuSelection} fa-10x style="color:#05386b></i>
+            <p><strong>CPU Chose ${cpuSelection}</strong></p>
+        `;
     } else {
-        alert("It's TIE!")
+        modalResult.innerHTML = `
+            <h1>It's a DRAW!</h1>
+            <i class="choice fas fa-hand-${cpuSelection} fa-10x style="color:#05386b"></i>
+            <p><strong>CPU Chose ${cpuSelection}</strong></p>
+        `;
+    }
+
+    // Show modal
+    modal.style.display = "block";
+}
+
+// Reset scores
+function resetScores(scoreboard) {
+    if ((scoreboard.player==5) || (scoreboard.cpu==5)) {
+        cpuScoreIncrement.innerHTML = `<p class="cpu-score">0</p>`;
+        playerScoreIncrement.innerHTML = `<p class="player-score">0</p>`;
     }
 }
 
-function game() {
-    while(cpuScore!=5 && playerScore!=5) {
-        playRound(playerSelection, cpuSelection)
+// Clear modal
+function clearModal(e) {
+    if(e.target == modal) {
+        modal.style.display = "none";
     }
 }
+
+// Event listeners
+choices.forEach(choice => choice.addEventListener("click", play));
+window.addEventListener("click", clearModal)
